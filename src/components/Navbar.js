ec2e0fc1be5c04c689459dca79c90a0252894856
Srihari Sreenath <52415086+Sriharivv@users.noreply.github.com>
2020-10-24 16:12:58 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../logo.svg";
 import {Navbar,Nav,NavItem,Button,Container,Row,Col,Dropdown} from "react-bootstrap";
 import ReactSearchBox from "react-search-box";
 import {detailProduct,storeProducts} from "../data";
@@ -9,9 +8,7 @@ import { ProductConsumer } from "../context";
 import "../App.css";
 import {faMobile} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 export default class Navbar1 extends Component {
-
   render() {
     return (
       <ProductConsumer>
@@ -19,67 +16,48 @@ export default class Navbar1 extends Component {
           const {bool}=value;
           const {signup}=value;
           return(
-            <Navbar  className="Col"  expand="lg">
-               <Container>
-                <Row>
+          <Navbar  className="Col"  expand="lg">
+            <Container>
+              <Row>
                   <Link to="/" style={{ textDecoration: 'none' }}>
-                  <Col className="Fnt1" >
-                  Amazona
-                  </Col>
+                   <Col className="Fnt1" >
+                   BuyAPhone
+                   </Col>
                   </Link>
-                </Row>
-                <Row className="SearchBar" >
-
+              </Row>
+              <Row className="SearchBar" >
                   <input type="text" placeholder="" expandable="true" />
-
-                 </Row>
-                 <Row>
+              </Row>
+              <Row>
                  {bool?
-                <Link to=""/>
-                :
-                <Link to="/cart" style={{ textDecoration: 'none' }}>
+                 <Link to=""/>
+                 :
+                 <Link to="/cart" style={{ textDecoration: 'none' }}>
                     <Col className="Fnt2 ">
                         Cart
                      </Col>
                  </Link>}
-                  </Row>
-                         <Row>
-                             {bool?
-                            <Link to="/login" style={{ textDecoration: 'none' }}>
-                               <Col className="Fnt4" >
-                                Login
-                               </Col>
-                            </Link>
+              </Row>
+              <Row>
+                   {bool?
+                      <Link to="/login" style={{ textDecoration: 'none' }}>
+                          <Col className="Fnt4" >
+                              Login
+                           </Col>
+                      </Link>
                             :
-                            <Link  style={{ textDecoration: 'none' }}>
-                            <Col className="Fnt4" onClick={value.showModal} style={{ textDecoration: 'none' }}>
+                      <Link  style={{ textDecoration: 'none' }}>
+                          <Col className="Fnt4" onClick={value.showModal} style={{ textDecoration: 'none' }}>
                             Log Out
-                            </Col>
-                            </Link>
-                          }
-
-                          </Row>
-                            </Container>
-                         </Navbar>
+                          </Col>
+                      </Link>
+                    }
+                </Row>
+            </Container>
+          </Navbar>
           )
         }}
       </ProductConsumer>
               );
             }
           }
-
-
-
-          const HoverButton = styled.button`
-          	color: Grey;
-            background-color:darkBlue;
-            font-size:1.7em;
-            border-radius:0.3em;
-
-
-          	:hover {
-          		background-color:#736AFF;
-
-
-          	}
-          `
